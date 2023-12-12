8d747305e0c269cddd6cf6de7718a8d22f1deb90
Gitanjali M <gitanjali.kotwal@gmail.com>
2023-12-12 15:30:46 -0600

    Basic funtionality set

---
+++ flaskblog.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "<h1>Hello World</h1>"
+
+@app.route("/about")
+def about():
+	return "<h1>About Page</h1>"
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
