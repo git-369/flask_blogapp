60341d3dae44ee19cc708853e69eeb78f881c568
Gitanjali M <gitanjali.kotwal@gmail.com>
2023-12-12 15:39:07 -0600

    rendering static html pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "<h1>Hello World</h1>"
+	return render_template('home.html')
 
 @app.route("/about")
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html')
 
 
 
