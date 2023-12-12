afb0f5abbe621eba0f3a07c5b3d4c9ea07e7ebbd
Gitanjali M <gitanjali.kotwal@gmail.com>
2023-12-12 15:53:47 -0600

    rendering dynamic content

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+posts = [
+	{
+	'title':'Blog 1',
+	'author': 'Gitanjali M',
+	'content': 'First Blog post content',
+	'date_posted':'12 december 2023'
+	},
+	{
+	'title':'Blog 2',
+	'author': 'Gitanjali M',
+	'content': 'Second Blog post content',
+	'date_posted':'15 december 2023'
+	}
+]
+
 
 @app.route("/")
 def hello():
-	return render_template('home.html')
+	return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-	return render_template('about.html')
+	return render_template('about.html',title="About")
 
 
 
